0c98d612f36983453bc1289a674678d0a143f167
Sang Bui <xsang.bui@gmail.com>
2020-04-08 22:36:53 +0700

    init frontend

---
+++ src/include/footer.php
@@ -0,0 +1,6 @@
+
+  <footer style="position: absolute; bottom: 0">
+    <div class="container">
+        <p>Copyright @abc</p>
+      </div>
+  </footer>
---
+++ src/index.php
@@ -0,0 +1,11 @@
+<?php
+include_once 'include/header.php';
+?>
+<body>
+	<div class="container" style="margin-top:100px">
+    <? include_once 'include/navbar.php' ?>
+    <h1>Hello</h1>
+  </div>
+  <? include_once 'include/footer.php' ?>
+</body>
+</html>
