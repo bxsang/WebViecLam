0301b3290a76f991c015bc89f2c85e96b25526d5
Sang Bui <xsang.bui@gmail.com>
2020-05-23 02:40:02 +0700

    add employee account registration

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -23,3 +23,28 @@ function handleLogin(response) {
     $("#wrong_credentials").css('display', 'block');
   }
 }
+
+$(".form-signup_ntv").submit(function (event) {
+  event.preventDefault();
+
+  let form = $(this);
+  let url = 'http://127.0.0.1/api/';
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: form.serialize(),
+    success: function (response) {
+      handleRegister(response);
+    }
+  });
+});
+
+function handleRegister(response) {
+  if (response.status === 'success') {
+    console.log("Employee registration success!!!");
+    $("#signup_ntv").modal('toggle');
+  } else {
+    console.log("Employee registration failed");
+  }
+}
