5eacc6cc05eb909d8187095c9fcc14e99aa8807a
Sang Bui <xsang.bui@gmail.com>
2020-05-25 13:57:53 +0700

    cleanup search

--- src/search/index.php
+++ src/search/index.php
@@ -22,25 +22,9 @@ if (isset($_REQUEST['q']) || isset($_REQUEST['city'])) {
   ?>
 
   <div class="container mt68">
-    <h1>Hello</h1>
     <div class="container search-container">
       <h3 class="text-center">Công việc mới đăng</h3>
       <div class="row list-job">
-      <div class="col-6">
-        <div class="card">
-          <div class="card-body">
-            <div class="logo-box">
-              <img src="https://salt.topdev.vn/JlorGxjbwWuLgupzcV2BewxBjpQlYLnCYf9my4-qpv4/fit/120/0/ce/1/aHR0cHM6Ly9hc3NldHMudG9wZGV2LnZuL2ZpbGVzL2xvZ29zL2I4ZGIzYTBhMjE4NzdhOGQ4Y2ZhODEwM2EyNmFhM2FlLmpwZw/b8db3a0a21877a8d8cfa8103a26aa3ae.jpg">
-            </div>
-            <div class="job-content">
-              <a target="_blank" href="#">
-                <strong><h4>Tuyen lap trinh vien PHP</h4></strong>
-              </a>
-              <a href="#"><em>Cong ty A</em></a>
-            </div>
-          </div>
-        </div>
-      </div>
 
       <?php
         foreach ($jobs as $key => $value) {
