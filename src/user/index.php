64a9ffd6a9737a79f01ccea304b29d9bd9409f8a
Sang Bui <xsang.bui@gmail.com>
2020-05-25 21:02:57 +0700

    add employer profile page section

--- src/user/index.php
+++ src/user/index.php
@@ -1,5 +1,13 @@
 <?php
+  // require_once $_SERVER['DOCUMENT_ROOT'].'/components/auth.php';
 
+  // $auth = new Auth();
+  // $auth->getTokenFromClient();
+
+  // if (!$auth->isLoggedIn()) {
+  //   echo 'Bạn chưa đăng nhập';
+  //   die();
+  // }
 ?>
 
 <?php include_once '../include/header.html'; ?>
@@ -25,21 +33,29 @@
             </div>
 
             <div class="list-group menu-employee" role="tablist">
-              <a class="list-group-item list-group-item-action active" id="list-profile-list" data-toggle="list" href="#list-profile"
+              <a class="list-group-item list-group-item-action active" id="list-employee-profile-list" data-toggle="list" href="#list-employee-profile"
                 role="tab" aria-controls="home">Thông tin cá nhân</a>
+
+              <a class="list-group-item list-group-item-action" id="list-employer-profile-list" data-toggle="list" href="#list-employer-profile"
+                role="tab" aria-controls="home">Thông tin nhà tuyển dụng</a>
+
               <a class="list-group-item list-group-item-action" id="list-applied-jobs-list" data-toggle="list" href="#list-applied-jobs"
                 role="tab" aria-controls="profile">Việc làm đã ứng tuyển</a>
-              <a class="list-group-item list-group-item-action" id="list-search-jobs-list" data-toggle="list" href="#list-search-jobs"
-                role="tab" aria-controls="messages">Tìm kiếm việc làm</a>
               <a class="list-group-item list-group-item-action" id="list-saved-jobs-list" data-toggle="list" href="#list-saved-jobs"
                 role="tab" aria-controls="settings">Việc làm đã lưu</a>
+              <a class="list-group-item list-group-item-action" id="list-search-jobs-list" data-toggle="list" href="#list-search-jobs"
+                role="tab" aria-controls="messages">Tìm kiếm việc làm</a>
+
+              <a class="list-group-item list-group-item-action" id="list-manage-jobs-list" data-toggle="list" href="#list-manage-jobs"
+                role="tab" aria-controls="settings">Quản lý công việc</a>
+
             </div>
           </div>
         </div>
 
         <div class="col-lg-8 pb-5">
           <div class="tab-content" id="nav-tabContent">
-            <div class="tab-pane fade show active" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">
+            <div class="tab-pane fade show active" id="list-employee-profile" role="tabpanel" aria-labelledby="list-employee-profile-list">
               <h1>Chỉnh sửa thông tin cá nhân</h1>
               <form class="row">
                 <div class="col-md-6">
@@ -72,8 +88,83 @@
               </form>
             </div>
 
+            <div class="tab-pane fade" id="list-employer-profile" role="tabpanel" aria-labelledby="list-employer-profile-list">
+              <h1>Chỉnh sửa thông tin nhà tuyển dụng</h1>
+              <form class="row">
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <label>Email</label>
+                    <input type="email" name="email" class="form-control" placeholder="Email" required="">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <label>Tên công ty</label>
+                    <input type="text" name="company_name" class="form-control" placeholder="Tên công ty" required="">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <label>Tỉnh/thành hoạt động</label>
+                  <select id="inputCompanyProvice" name="address" class="custom-select mb-3">
+                    <option value="hn">Hà Nội</option>
+                    <option value="dn">Đà Nẵng</option>
+                    <option value="hcm">Hồ Chí Minh</option>
+                  </select>
+                </div>
+                <div class="col-md-6">
+                  <label>Quy mô nhân sự</label>
+                  <select id="inputCompanyScale" name="scale" class="custom-select mb-3">
+                    <option value="1">Dưới 20 người</option>
+                    <option value="2">20-150 người</option>
+                    <option value="3">150-300 người</option>
+                    <option value="4">Trên 300 người</option>
+                  </select>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <label>Tên người liên hệ</label>
+                    <input type="text" id="inputCompanyRepresentative" name="contact_name" class="form-control" placeholder="Tên người liên hệ" required="">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <label>SĐT liên hệ</label>
+                    <input type="text" id="inputCompanyPhone" name="contact_phone" class="form-control" placeholder="SĐT liên hệ" required="">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="form-label-group">
+                    <label>Email liên hệ</label>
+                    <input type="email" id="inputCompanyEmail" name="contact_email" class="form-control" placeholder="Email liên hệ" required="">
+                  </div>
+                </div>
+                <div class="col-md-12 d-flex justify-content-center">
+                  <button class="btn btn-rounded btn-lg blue-gradient text-uppercase" type="submit">Cập nhật</button>
+                </div>
+              </form>
+            </div>
+  
             <div class="tab-pane fade" id="list-applied-jobs" role="tabpanel" aria-labelledby="list-applied-jobs-list">
               <h1>Việc làm đã ứng tuyển</h1>
+              <div class="row list-job">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="logo-box">
+                      <img src="https://salt.topdev.vn/JlorGxjbwWuLgupzcV2BewxBjpQlYLnCYf9my4-qpv4/fit/120/0/ce/1/aHR0cHM6Ly9hc3NldHMudG9wZGV2LnZuL2ZpbGVzL2xvZ29zL2I4ZGIzYTBhMjE4NzdhOGQ4Y2ZhODEwM2EyNmFhM2FlLmpwZw/b8db3a0a21877a8d8cfa8103a26aa3ae.jpg">
+                    </div>
+                    <div class="job-content">
+                      <a target="_blank" href="#">
+                        <strong><h4>Tuyển lập trình viên PHP</h4></strong>
+                      </a>
+                      <a href="#"><em>Công ty A</em></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="tab-pane fade" id="list-saved-jobs" role="tabpanel" aria-labelledby="list-saved-jobs-list">
+              <h1>Việc làm đã lưu</h1>
               <div class="row list-job">
                 <div class="card">
                   <div class="card-body">
@@ -107,8 +198,11 @@
                 </form>
             </div>
 
-            <div class="tab-pane fade" id="list-saved-jobs" role="tabpanel" aria-labelledby="list-saved-jobs-list">
-              <h1>Việc làm đã lưu</h1>
+            <div class="tab-pane fade" id="list-manage-jobs" role="tabpanel" aria-labelledby="list-manage-jobs-list">
+              <div id="manage_job_title">
+                <h1>Quản lý công việc</h1>
+                <button class="btn btn-success btn-rounded"><i class="fas fa-plus mr-1"></i> Tạo mới</button>
+              </div>
               <div class="row list-job">
                 <div class="card">
                   <div class="card-body">
